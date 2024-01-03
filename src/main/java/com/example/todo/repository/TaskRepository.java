5030026c3f2b1d1a855e689dd49b853db8c94aef
harika234 <harikacdm@gmail.com>
2024-01-03 23:00:07 +0530

    searchByCategoryName

--- src/main/java/com/example/todo/repository/TaskRepository.java
+++ src/main/java/com/example/todo/repository/TaskRepository.java
@@ -2,13 +2,17 @@ package com.example.todo.repository;
 
 import com.example.todo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long>  {
-    public  Task fetchTaskByName(String taskName);
+//    @Query("SELECT t FROM Task t WHERE t.taskName = :taskName")
+//    public  Task fetchTaskByName(String taskName);
 
 //    public Task findByTaskIgnoreCase(String taskName);
+//public Task findByTaskNameIgnoreCase(String taskName);
+
 
     public Task findByTaskNameIgnoreCase(String taskName);
 }
